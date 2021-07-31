f4dc5412eae9b4011a79adf366f1210affac3d29
rahmor <rahmor@yahoo.com>
2021-07-30 20:06:44 -0400

    add AlaCarte component test

--- src/tests/AlaCarte.test.js
+++ src/tests/AlaCarte.test.js
@@ -24,35 +24,22 @@ describe('AlaCarte component', () => {
   });
 
   it('should have channels to select', () => {
-    AuthApiService.channelsService = () => {
-      return Promise.resolve({
-        channels: [
-          {
-            id: 1,
-            channel: 'HBO',
-            price: 5,
-            tier: 'Premium',
-          },
-          {
-            id: 2,
-            channel: 'Showtime',
-            price: 5,
-            tier: 'Premium',
-          },
-        ],
-      });
-    };
-
     wrapper.update();
     const select = wrapper.find('option').at(1);
-    console.log(select.debug());
     expect(select.text()).toEqual('HBO');
   });
-  it('should list selected channels and have correct price', () => {
-    //find select options
-    // select hbo option inside an act
-    //update wrapper
-    //make sure channel now shows
-    //make sure price is correct
+  it('should list selected channels and have correct price', async () => {
+    await wrapper.update();
+
+    const formControl = wrapper.find('FormControl').at(0);
+
+    await act(async () => {
+      formControl.simulate('change', { target: { value: 'Cinemax' } });
+    });
+
+    await wrapper.update();
+
+    expect(wrapper.find('ToastBody').length).toBe(1);
+    expect(wrapper.find('.navbar-text').text()).toBe('$5');
   });
 });
